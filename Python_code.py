8f616661acceb2ec7da563764aa9a0f9b9befa7e
Hassan Williams <hassanw@email.uscb.edu>
2025-11-16 13:21:19 -0500

    found correlation within the data

--- Python_code.py
+++ Python_code.py
@@ -1,24 +1,27 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-#loading data
-# clean_lines = []
-# with open('Yssrb_data.txt', 'r') as f:
-#     for line in f:
-#         clean_line = line.replace(',', '')  # remove commas
-#         clean_lines.append(clean_line)
 
-# # Optionally save cleaned data
-# with open('Yssrb_data_clean.txt', 'w') as f:
-#     f.writelines(clean_lines)
+# importing data 
 df = pd.read_csv('Yssrb_data.txt', delimiter=',', header = 0)
 df.head()
+# converting any string types into numeric values
+df = df.apply(pd.to_numeric, errors='coerce')
 
+corr = df.corr()
+print(corr)
 
+# looping throrugh every pair of columns to check if they correlate
+for col1 in corr.columns:
+    for col2 in corr.columns:
+        if col1 == col2:
+            continue # skips self  correlations
+        value = corr.loc[col1, col2]
+        # checking for strong correlations
+        if abs(value) > 0.5:
+            print(f'{col1} and {col2}: STRONG correlation ({value: .2f})')
+        # checking for weak correlations
+        else:
+            print(f'{col1} and {col2}: WEAK correlation ({value: .2f})')
+        
 
-
-print(df.head())
-
-#df = df.replace(',', '', regex=True)
-# for line in df:
-#     parts = line.strip().split(',')
+ 
